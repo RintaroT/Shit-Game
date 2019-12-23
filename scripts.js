80f5192368a530fe243367eaf4f029388d352015
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-12-22 18:58:27 -0600

    j

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
 var x = 0;
 var y = 0;
-var tree = ["x-3y4"];
+var tree = [];
 
 var sheet = (function() {
-	// Create the <style> tag
-	var style = document.createElement("style");
+  // Create the <style> tag
+  var style = document.createElement("style");
 
-	// Add a media (and/or media query) here if you'd like!
-	// style.setAttribute("media", "screen")
-	// style.setAttribute("media", "only screen and (max-width : 1024px)")
+  // Add a media (and/or media query) here if you'd like!
+  // style.setAttribute("media", "screen")
+  // style.setAttribute("media", "only screen and (max-width : 1024px)")
 
-	// WebKit hack :(
-	style.appendChild(document.createTextNode(""));
+  // WebKit hack :(
+  style.appendChild(document.createTextNode(""));
 
-	// Add the <style> element to the page
-	document.head.appendChild(style);
+  // Add the <style> element to the page
+  document.head.appendChild(style);
 
-	return style.sheet;
+  return style.sheet;
 })();
 document.getElementById("body").onload = sheet;
 sheet.addRule("#l12", "background: white;", 0);
@@ -26,65 +26,65 @@ for (var i = 0; i < 5000; i++) {
   var treex = Math.round(Math.random() * 500);
   var treey = Math.round(Math.random() * 500);
   tree[tree.length] = "x" + treex + "y" + treey;
- treex++;
+  treex++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex--;
-   treex--;
+  treex--;
+  treex--;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex++;
-   treey++;
+  treex++;
+  treey++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treey--;
-   treey--;
+  treey--;
+  treey--;
   tree[tree.length] = "x" + treex + "y" + treey;
 
   var treex = Math.round(Math.random() * -500);
   var treey = Math.round(Math.random() * 500);
   tree[tree.length] = "x" + treex + "y" + treey;
- treex++;
+  treex++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex--;
-   treex--;
+  treex--;
+  treex--;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex++;
-   treey++;
+  treex++;
+  treey++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treey--;
-   treey--;
+  treey--;
+  treey--;
   tree[tree.length] = "x" + treex + "y" + treey;
 
   var treex = Math.round(Math.random() * 500);
   var treey = Math.round(Math.random() * -500);
   tree[tree.length] = "x" + treex + "y" + treey;
- treex++;
+  treex++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex--;
-   treex--;
+  treex--;
+  treex--;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex++;
-   treey++;
+  treex++;
+  treey++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treey--;
-   treey--;
+  treey--;
+  treey--;
   tree[tree.length] = "x" + treex + "y" + treey;
 
   var treex = Math.round(Math.random() * -500);
   var treey = Math.round(Math.random() * -500);
   tree[tree.length] = "x" + treex + "y" + treey;
- treex++;
+  treex++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex--;
-   treex--;
+  treex--;
+  treex--;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treex++;
-   treey++;
+  treex++;
+  treey++;
   tree[tree.length] = "x" + treex + "y" + treey;
-   treey--;
-   treey--;
+  treey--;
+  treey--;
   tree[tree.length] = "x" + treex + "y" + treey;
 }
 for (var i = 0; i < tree.length; i++) {
-sheet.addRule("." + tree[i], "background: green;", 0);
+  sheet.addRule("." + tree[i], "background: green;", 0);
 }
 
 
@@ -149,28 +149,28 @@ document.getElementById("body").onload = function grid() {
   for (var i = 0; i < 23; i++) {
     ii = i + 1;
     var cy = 11 - i + y;
-xa = x - 11;
-xb = x - 10;
-xc = x - 9;
-xd = x - 8;
-xe = x - 7;
-xf = x - 6;
-xg = x - 5;
-xh = x - 4;
-xi = x - 3;
-xj = x - 2;
-xk = x - 1;
-xm = x + 1;
-xn = x + 2;
-xo = x + 3;
-xp = x + 4;
-xq = x + 5;
-xr = x + 6;
-xs = x + 7;
-xt = x + 8;
-xu = x + 9;
-xv = x + 10;
-xw = x + 11;
+    xa = x - 11;
+    xb = x - 10;
+    xc = x - 9;
+    xd = x - 8;
+    xe = x - 7;
+    xf = x - 6;
+    xg = x - 5;
+    xh = x - 4;
+    xi = x - 3;
+    xj = x - 2;
+    xk = x - 1;
+    xm = x + 1;
+    xn = x + 2;
+    xo = x + 3;
+    xp = x + 4;
+    xq = x + 5;
+    xr = x + 6;
+    xs = x + 7;
+    xt = x + 8;
+    xu = x + 9;
+    xv = x + 10;
+    xw = x + 11;
     document.getElementById('a' + ii).setAttribute("class", "x" + xa + "y" + cy);
     document.getElementById('b' + ii).setAttribute("class", "x" + xb + "y" + cy);
     document.getElementById('c' + ii).setAttribute("class", "x" + xc + "y" + cy);
@@ -200,18 +200,38 @@ xw = x + 11;
 
 document.getElementById("body").onkeypress = function move(event) {
   var key = event.which || event.keyCode;
-//  console.log(key);
+  //  console.log(key);
   if (key == 119) {
-    y++
+    var n = y + 1;
+    var up = "x" + x + "y" + n;
+    var f = window.getComputedStyle(document.getElementsByClassName(up)[0]).getPropertyValue('background-Color');
+    if (f !== "rgb(0, 128, 0)") {
+      y++
+    }
   }
   if (key == 100) {
-    x++
+    var n = x + 1;
+    var right = "x" + n + "y" + y;
+    var f = window.getComputedStyle(document.getElementsByClassName(right)[0]).getPropertyValue('background-Color');
+    if (f !== "rgb(0, 128, 0)") {
+      x++
+    }
   }
   if (key == 97) {
-    x--
+    var n = x - 1;
+    var left = "x" + n + "y" + y;
+    var f = window.getComputedStyle(document.getElementsByClassName(left)[0]).getPropertyValue('background-Color');
+    if (f !== "rgb(0, 128, 0)") {
+      x--
+    }
   }
   if (key == 115) {
-    y--
+    var n = y - 1;
+    var down = "x" + x + "y" + n;
+    var f = window.getComputedStyle(document.getElementsByClassName(down)[0]).getPropertyValue('background-Color');
+    if (f !== "rgb(0, 128, 0)") {
+      y--
+    }
   }
   for (var i = 0; i < 23; i++) {
     ii = i + 1;
@@ -238,29 +258,29 @@ document.getElementById("body").onkeypress = function move(event) {
     xu = x + 9;
     xv = x + 10;
     xw = x + 11;
-        document.getElementById('a' + ii).setAttribute("class", "x" + xa + "y" + cy);
-        document.getElementById('b' + ii).setAttribute("class", "x" + xb + "y" + cy);
-        document.getElementById('c' + ii).setAttribute("class", "x" + xc + "y" + cy);
-        document.getElementById('d' + ii).setAttribute("class", "x" + xd + "y" + cy);
-        document.getElementById('e' + ii).setAttribute("class", "x" + xe + "y" + cy);
-        document.getElementById('f' + ii).setAttribute("class", "x" + xf + "y" + cy);
-        document.getElementById('g' + ii).setAttribute("class", "x" + xg + "y" + cy);
-        document.getElementById('h' + ii).setAttribute("class", "x" + xh + "y" + cy);
-        document.getElementById('i' + ii).setAttribute("class", "x" + xi + "y" + cy);
-        document.getElementById('j' + ii).setAttribute("class", "x" + xj + "y" + cy);
-        document.getElementById('k' + ii).setAttribute("class", "x" + xk + "y" + cy);
-        document.getElementById('l' + ii).setAttribute("class", "x" + x + "y" + cy);
-        document.getElementById('m' + ii).setAttribute("class", "x" + xm + "y" + cy);
-        document.getElementById('n' + ii).setAttribute("class", "x" + xn + "y" + cy);
-        document.getElementById('o' + ii).setAttribute("class", "x" + xo + "y" + cy);
-        document.getElementById('p' + ii).setAttribute("class", "x" + xp + "y" + cy);
-        document.getElementById('q' + ii).setAttribute("class", "x" + xq + "y" + cy);
-        document.getElementById('r' + ii).setAttribute("class", "x" + xr + "y" + cy);
-        document.getElementById('s' + ii).setAttribute("class", "x" + xs + "y" + cy);
-        document.getElementById('t' + ii).setAttribute("class", "x" + xt + "y" + cy);
-        document.getElementById('u' + ii).setAttribute("class", "x" + xu + "y" + cy);
-        document.getElementById('v' + ii).setAttribute("class", "x" + xv + "y" + cy);
-        document.getElementById('w' + ii).setAttribute("class", "x" + xw + "y" + cy);
+    document.getElementById('a' + ii).setAttribute("class", "x" + xa + "y" + cy);
+    document.getElementById('b' + ii).setAttribute("class", "x" + xb + "y" + cy);
+    document.getElementById('c' + ii).setAttribute("class", "x" + xc + "y" + cy);
+    document.getElementById('d' + ii).setAttribute("class", "x" + xd + "y" + cy);
+    document.getElementById('e' + ii).setAttribute("class", "x" + xe + "y" + cy);
+    document.getElementById('f' + ii).setAttribute("class", "x" + xf + "y" + cy);
+    document.getElementById('g' + ii).setAttribute("class", "x" + xg + "y" + cy);
+    document.getElementById('h' + ii).setAttribute("class", "x" + xh + "y" + cy);
+    document.getElementById('i' + ii).setAttribute("class", "x" + xi + "y" + cy);
+    document.getElementById('j' + ii).setAttribute("class", "x" + xj + "y" + cy);
+    document.getElementById('k' + ii).setAttribute("class", "x" + xk + "y" + cy);
+    document.getElementById('l' + ii).setAttribute("class", "x" + x + "y" + cy);
+    document.getElementById('m' + ii).setAttribute("class", "x" + xm + "y" + cy);
+    document.getElementById('n' + ii).setAttribute("class", "x" + xn + "y" + cy);
+    document.getElementById('o' + ii).setAttribute("class", "x" + xo + "y" + cy);
+    document.getElementById('p' + ii).setAttribute("class", "x" + xp + "y" + cy);
+    document.getElementById('q' + ii).setAttribute("class", "x" + xq + "y" + cy);
+    document.getElementById('r' + ii).setAttribute("class", "x" + xr + "y" + cy);
+    document.getElementById('s' + ii).setAttribute("class", "x" + xs + "y" + cy);
+    document.getElementById('t' + ii).setAttribute("class", "x" + xt + "y" + cy);
+    document.getElementById('u' + ii).setAttribute("class", "x" + xu + "y" + cy);
+    document.getElementById('v' + ii).setAttribute("class", "x" + xv + "y" + cy);
+    document.getElementById('w' + ii).setAttribute("class", "x" + xw + "y" + cy);
   }
-document.getElementById("l12").style.backgroundColor = 'white';
+  document.getElementById("l12").style.backgroundColor = 'white';
 }
